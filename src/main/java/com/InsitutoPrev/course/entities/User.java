dc42d1787403ad01290a2faf9763b41ddaf7343d
Airton Schrenk <airtonnigolino@gmail.com>
2020-07-21 23:00:52 -0300

    H2 database, test profile, JPA

--- src/main/java/com/InsitutoPrev/course/entities/User.java
+++ src/main/java/com/InsitutoPrev/course/entities/User.java
@@ -2,9 +2,17 @@ package com.InsitutoPrev.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	//implantar atributos básicos
+	@Id//chave primária
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
